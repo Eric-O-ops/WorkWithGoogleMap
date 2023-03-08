6b9ed262737d7e51cb40dd7f727612b026e00838
Omaraliev Eric <theslyfox43@gmail.com>
2023-03-08 13:17:17 +0600

    di for firestore

---
+++ app/src/main/java/com/geektech/workwithgooglemap/di/FirestoreModule.kt
@@ -0,0 +1,20 @@
+package com.geektech.workwithgooglemap.di
+
+import com.geektech.workwithgooglemap.data.remote.repositories.UserTestRepository
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirestoreModule {
+
+    @Provides
+    fun provideFirestore() = Firebase.firestore
+
+    @Provides
+    fun provideUserTestRep() = UserTestRepository(provideFirestore())
+}
