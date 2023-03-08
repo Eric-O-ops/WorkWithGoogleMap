219f18f5993a0b535406bfbb8bb88e8d707bca44
Omaraliev Eric <theslyfox43@gmail.com>
2023-03-08 13:17:39 +0600

    convert image to bitmap

---
+++ app/src/main/java/com/geektech/workwithgooglemap/data/remote/repositories/ConvertToBitmap.kt
@@ -0,0 +1,34 @@
+package com.geektech.workwithgooglemap.data.remote.repositories
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+
+interface ConvertToBitmap {
+
+    fun convert(context: Context, image: Int): BitmapDescriptor
+
+    class Base: ConvertToBitmap {
+        override fun convert(context: Context, image: Int): BitmapDescriptor {
+            val vectorDrawable = ContextCompat.getDrawable(context, image)
+            vectorDrawable?.setBounds(
+                0,
+                0,
+                vectorDrawable.intrinsicWidth,
+                vectorDrawable.intrinsicHeight
+            )
+            val bitmap = Bitmap.createBitmap(
+                vectorDrawable!!.intrinsicWidth,
+                vectorDrawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+
+            val canvas = Canvas(bitmap)
+            vectorDrawable.draw(canvas)
+            return BitmapDescriptorFactory.fromBitmap(bitmap)
+        }
+    }
+}
