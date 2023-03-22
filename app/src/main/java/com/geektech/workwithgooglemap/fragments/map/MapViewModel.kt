22305892694951355fccacc240cb3d6d0f6a4720
Omaraliev Eric <theslyfox43@gmail.com>
2023-03-22 23:20:00 +0600

    updated MapViewModel.kt

--- app/src/main/java/com/geektech/workwithgooglemap/fragments/map/MapViewModel.kt
+++ app/src/main/java/com/geektech/workwithgooglemap/fragments/map/MapViewModel.kt
@@ -1,16 +1,19 @@
-package com.geektech.workwithgooglemap.fragments
+package com.geektech.workwithgooglemap.fragments.map
 
+import android.location.Location
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.geektech.workwithgooglemap.data.remote.repositories.UserTestRepository
-import com.geektech.workwithgooglemap.data.remote.repositories.Users
+import com.geektech.workwithgooglemap.fragments.Users
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class BlankViewModel @Inject constructor(
+class MapViewModel @Inject constructor(
     private val repository: UserTestRepository,
     private val firestore: FirebaseFirestore
 ) : ViewModel() {
@@ -18,8 +21,16 @@ class BlankViewModel @Inject constructor(
     private var _users: MutableLiveData<ArrayList<Users>> = MutableLiveData()
 
     init {
+        viewModelScope.launch {
+            listenUpdatesUsersLocation()
 
-        listenUpdatesUsersLocation()
+        }
+    }
+
+
+    fun updateThisUserLocation(name: String, location: Location) {
+
+        repository.updateThisUserLocation(name, location)
     }
 
     private fun listenUpdatesUsersLocation() {
@@ -32,15 +43,24 @@ class BlankViewModel @Inject constructor(
 
                 val allUsers: ArrayList<Users> = ArrayList()
                 for (doc in value!!) {
-                    val name = doc.getString("name")
-                    val geoPoint = doc.getGeoPoint("location")
-                    allUsers.add(Users(name!!, geoPoint!!))
-
+                    if(doc.getString("name") != "marsel") {
+                        allUsers.add(
+                            Users(
+                                doc.getString("name")!!,
+                                doc.getGeoPoint("location")!!
+                            )
+                        )
+                    }
                 }
                 _users.value = allUsers
             }
     }
+
     internal var user: MutableLiveData<ArrayList<Users>>
-        get() { return _users }
-        set(value) {_users = value}
+        get() {
+            return _users
+        }
+        set(value) {
+            _users = value
+        }
 }
