a8baded68b750c3f9d787021e5c570e4eb2578aa
Omaraliev Eric <theslyfox43@gmail.com>
2023-03-22 23:08:56 +0600

    removed classes

--- app/src/main/java/com/geektech/workwithgooglemap/data/remote/repositories/Users.kt
+++
@@ -1,8 +0,0 @@
-package com.geektech.workwithgooglemap.data.remote.repositories
-
-import com.google.firebase.firestore.GeoPoint
-
-data class Users(
-    val name: String,
-    var location: GeoPoint
-)
