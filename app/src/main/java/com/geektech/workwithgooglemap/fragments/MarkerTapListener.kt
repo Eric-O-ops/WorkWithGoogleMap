4ef72f8ac7808bea0fc0738c015bea24e9b2c506
Omaraliev Eric <theslyfox43@gmail.com>
2023-03-23 20:41:35 +0600

    renamed

--- app/src/main/java/com/geektech/workwithgooglemap/fragments/MarkerTapListener.kt
+++ app/src/main/java/com/geektech/workwithgooglemap/fragments/MarkerTapListener.kt
@@ -3,11 +3,11 @@ package com.geektech.workwithgooglemap.fragments
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 
-class MarkerListener(
+class MarkerTapListener(
     private val mMap: GoogleMap
 ) {
 
-    fun markerListener() {
+   operator fun invoke() {
         mMap.setOnMarkerClickListener { marker ->
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker.position, 18f))
             true
