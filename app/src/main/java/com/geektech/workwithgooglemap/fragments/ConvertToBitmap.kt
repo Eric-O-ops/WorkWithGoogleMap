a672a5ed062cfdb3217d73e9d30abe2addebf9ff
Omaraliev Eric <theslyfox43@gmail.com>
2023-03-23 20:42:10 +0600

    added new method convert from view to bitmap

--- app/src/main/java/com/geektech/workwithgooglemap/fragments/ConvertToBitmap.kt
+++ app/src/main/java/com/geektech/workwithgooglemap/fragments/ConvertToBitmap.kt
@@ -3,6 +3,7 @@ package com.geektech.workwithgooglemap.fragments
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.view.View
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
@@ -10,8 +11,9 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 interface ConvertToBitmap {
 
     fun convert(context: Context, image: Int): BitmapDescriptor
+    fun convert(view: View): Bitmap
 
-    class Base: ConvertToBitmap {
+    class Base : ConvertToBitmap {
         override fun convert(context: Context, image: Int): BitmapDescriptor {
             val vectorDrawable = ContextCompat.getDrawable(context, image)
             vectorDrawable?.setBounds(
@@ -29,6 +31,19 @@ interface ConvertToBitmap {
             val canvas = Canvas(bitmap)
             vectorDrawable.draw(canvas)
             return BitmapDescriptorFactory.fromBitmap(bitmap)
+        }
+
+        override fun convert(view: View): Bitmap {
+            view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+            val bitmap = Bitmap.createBitmap(
+                view.measuredWidth,
+                view.measuredHeight,
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bitmap)
+            view.layout(0, 0, view.measuredWidth, view.measuredHeight)
+            view.draw(canvas)
+            return bitmap
         }
     }
 }
